cfb2956bdd54d0c3be21a519b54634fed97ba9e0
Gabriel <gabrielmito054@gmail.com>
2022-05-09 20:44:09 -0300

    Aprendendo condições

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 17
+
+if(idade >= 18){
+    console.log('Vota')
+} else if(idade < 18 || idade > 70){
+    console.log('Voto não obrigatório')
+} else if (idade < 16){
+    console.log('Não vota')
+}
+    
